6a97ed80bbaf457d5a05385353c09e09776e6921
Ana Pascual <iamanmaria.pascual@gmail.com>
2016-04-19 00:56:24 +0400

    User Edit

--- user.control.php
+++ user.control.php
@@ -14,20 +14,17 @@
 	$hashedpw = md5( $salt.$password);
 
 	/*
-	 * SQL Query Insert User data
-	 */
-	$sql_insert = "INSERT INTO user (first_name,middle_name,last_name,username,password,rights)
-					VALUES ('$firstname','$middlename','$lastname','$email','$hashedpw','$user_role')";
-
-	/*
 	 * Save User Record
 	 */
-	
 	if (isset( $_POST['save'])) 
 	{
 		//Check if e-mail is already registered
 		$q = "SELECT * FROM user WHERE username = '$email'";
 		$result =  @mysqli_query($dbc, $q);
+
+		//SQL insert
+		$sql_insert = "INSERT INTO user (first_name,middle_name,last_name,username,password,rights)
+					VALUES ('$firstname','$middlename','$lastname','$email','$hashedpw','$user_role')";
 
 		if ( $result->num_rows > 0) {
 			echo '<div class="alert alert-danger" role="alert">E-mail entered already registered.</div>';
@@ -40,6 +37,32 @@
 		}
 		mysqli_close($dbc);
 	}//save
+
+	/*
+	 * Update User Record
+	 */
+	
+	if ( isset( $_GET['action'] ) ) {
+
+    	$id = $_GET['id'];
+	    $sql_query = "SELECT * FROM user WHERE user_id = '$id'";
+	    $result =  @mysqli_query($dbc, $sql_query); 
+
+	    if ( isset($_POST['user_update']) ) {
+			$sql_update = "UPDATE user SET first_name = '$firstname', middle_name = '$middlename', last_name = '$lastname', rights = '$user_role' WHERE user_id = '$id' ";
+			if (mysqli_query($dbc, $sql_update)) {
+				$result = @mysqli_query($dbc, $sql_query);
+	    		$msg = '<div class="alert alert-success" role="alert">User information has been updated successfully.</div>';
+			} else {
+				$msg = '<div class="alert alert-danger" role="alert">Error updating record: ' . mysqli_error($dbc) . '</div>';
+			}
+
+			mysqli_close($dbc);
+		}
+    }
+
+	
+
 
 	/*
 	 * Return role value
--- user_edit.php
+++ user_edit.php
@@ -1,10 +1,18 @@
-<?php include('header.php'); ?>
+<?php 
+	include('header.php');
+	require_once('mysqli_connect.php');
+    require_once( 'user.control.php' );
+?>
 	<div id="page-wrapper">
 		<div class="row">
 			<div class="col-lg-12">
 				<h1 class="page-header">Edit User</h1>
 			</div><!-- /.col-lg-12 -->
-
+		</div><!-- /.row -->
+		<div class="row">
+			<div class="col-lg-12">
+				<?php echo $msg; ?>
+			</div><!-- /.col-lg-12 -->
 		</div><!-- /.row -->
 		<div class="row">
 			<div class="col-lg-6">
@@ -15,31 +23,32 @@
 					<div class="panel-body">
 						<div class="row">
 							<div class="col-lg-12">
-								<form action="" method="post" role="form">
+								<form action="user_edit.php?action=edit&amp;id=<?php echo $id; ?>" method="post" role="form">
+								<?php while ( $row = $result->fetch_assoc()) { ?>
 									<div class="form-group">
 										<label>
 											First Name
 											<span class="asteriskField">*</span>
 										</label>
-										<input type="text" name="first_name" class="form-control" >
+										<input type="text" name="first_name" class="form-control" value="<?php echo $row['first_name']; ?>" >
 									</div>
 									<div class="form-group">
 										<label>Middle Name</label>
-										<input type="text" name="middle_name" class="form-control" >
+										<input type="text" name="middle_name" class="form-control" value="<?php echo $row['middle_name']; ?>">
 									</div>
 									<div class="form-group">
 										<label>
 											Last Name
 											<span class="asteriskField">*</span>
 										</label>
-										<input type="text" name="last_name" class="form-control" >
+										<input type="text" name="last_name" class="form-control" value="<?php echo $row['last_name']; ?>" >
 									</div>
 									<div class="form-group">
 										<label>
 											E-mail Address
 											<span class="asteriskField">*</span>
 										</label>
-										<input type="text" name="username" class="form-control" >
+										<input type="text" name="username" class="form-control" value="<?php echo $row['username']; ?>" readonly>
 										<p class="help-block">E-mail address will be used as your username.</p>
 									</div>
 									<div class="form-group">
@@ -47,36 +56,33 @@
 											Password
 											<span class="asteriskField">*</span>
 										</label>
-										<input type="password" name="username" class="form-control" >
+										<input type="password" name="username" class="form-control" value="<?php echo $row['password']; ?>" readonly>
 									</div>
 									<div class="form-group">
 										<label>Role</label>
 										<select name="user_role" class="form-control">
-											<option value="30">Staff</option>
-											<option value="20">Department Head</option>
-											<option value="10">Administrator</option>
-											<option value="0">Master Admin</option>
+											<?php
+												$roles = array('1', '2', '3', '4');
+												foreach ($roles as $role) {
+											      	$selected = ( $row['rights'] === $role ) ? ' selected="selected"' : '';
+												?>
+												    <option value="<?php echo $role; ?>" <?php echo $selected; ?>> <?php echo get_role( $role ); ?></option>
+											<?php } ?>
+
 										</select>
-									</div>
-									<div class="form-group">
-										<label>Active</label>
-										<label class="checkbox-inline">
-											<input type="checkbox" name="user_active">&nbsp;
-										</label>
 									</div>
 									<div class="row">
 										<div class="col-lg-4 col-lg-offset-4">
-											<button type="submit" name="user_save" class="btn btn-primary btn-block">Save</button>
+											<button type="submit" name="user_update" class="btn btn-primary btn-block">Update</button>
 										</div>
 										<div class="col-lg-4">
 											<button type="submit" name="user_save" class="btn btn-danger btn-block">Delete</button>
 										</div>
 									</div>
-
-
+									<?php } mysqli_close($dbc);?>
 								</form>
 							</div>
-						</div>
+					</div>
 					</div><!-- /.panel-body-->
 				</div><!-- /.panel panel-default-->
 			</div>
